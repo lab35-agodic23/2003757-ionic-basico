7d1e56565807d9c6223d03783d704a1d98608f9d
cesaralexanderds <123446917+PATRICIORICARDI@users.noreply.github.com>
2023-10-21 14:20:31 -0600

    practica 10 - rutas

--- miApp-2003757/src/app/app-routing.module.ts
+++ miApp-2003757/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { ProductosComponent } from './productos/productos.component';
+import { ProductoDetalleComponent } from './producto-detalle/producto-detalle.component';
 
 const routes: Routes = [
   {
@@ -8,8 +10,16 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: '/productos',
     pathMatch: 'full'
+  },
+  {
+    path: 'productos',
+    component: ProductosComponent,
+  },
+  {
+    path: 'producto/:id',
+    component: ProductoDetalleComponent,
   },
 ];
 
